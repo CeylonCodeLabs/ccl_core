1ea1017785e132ffc7876a63aa698f775d002d35
Pasan Eramusugoda <vaprera91@gmail.com>
2024-09-18 14:03:55 +0530

    refactor(Extension): Add nullable date time format extension

--- lib/src/extensions/date_time_extensions.dart
+++ lib/src/extensions/date_time_extensions.dart
@@ -98,3 +98,135 @@ extension DateTimeExtension on DateTime {
   String toHHmmss() => DateFormat('HH:mm').format(this);
 //endregion time
 }
+
+extension NullDateTimeExtension on DateTime? {
+  String? format(String pattern, [String? locale]) =>
+      this == null ? null : DateFormat(pattern, locale).format(this!);
+
+  //region date
+  //region date month year
+  /// Default => 2022-05-24 => 24-5-2022
+  String? toDMY([String? separator1 = '-', String? separator2 = '-']) =>
+      this == null
+          ? null
+          : DateFormat('d${separator1}M${separator1}y').format(this!);
+
+  /// Default => 2022-05-24 => 24-05-22
+  String? toDdMmYy([String? separator1 = '-', String? separator2 = '-']) =>
+      this == null
+          ? null
+          : DateFormat('dd${separator1}MM${separator2}yy').format(this!);
+
+  /// Default => 2022-05-24 => 24-05-2022
+  String? toDdMmYyyy([String? separator1 = '-', String? separator2 = '-']) =>
+      this == null
+          ? null
+          : DateFormat('dd${separator1}MM${separator2}yyyy').format(this!);
+
+  /// Default => 2022-05-24 => 24-May-22
+  String? toDdMmmYy([String? separator1 = '-', String? separator2 = '-']) =>
+      this == null
+          ? null
+          : DateFormat('dd${separator1}MMM${separator2}yy').format(this!);
+
+  /// Default => 2022-05-24 => 24-May-2022
+  String? toDdMmmYyyy([String? separator1 = '-', String? separator2 = '-']) =>
+      this == null
+          ? null
+          : DateFormat('dd${separator1}MMM${separator2}yyyy').format(this!);
+
+  //endregion
+
+  //region month date year
+  /// Default => 2022-05-24 => 5-24-2022
+  String? toMDY([String? separator1 = '-', String? separator2 = '-']) =>
+      this == null
+          ? null
+          : DateFormat('M${separator1}d${separator2}y').format(this!);
+
+  /// Default => 2022-05-24 => 05-24-22
+  String? toMmDdYy([String? separator1 = '-', String? separator2 = '-']) =>
+      this == null
+          ? null
+          : DateFormat('MM${separator1}dd${separator2}yy').format(this!);
+
+  /// Default => 2022-05-24 => May-24-2022
+  String? toMmDdYyyy([String? separator1 = '-', String? separator2 = '-']) =>
+      this == null
+          ? null
+          : DateFormat('MM${separator1}dd${separator2}yyyy').format(this!);
+
+  /// Default => 2022-05-24 => May-24-22
+  String? toMmmDdYy([String? separator1 = '-', String? separator2 = '-']) =>
+      this == null
+          ? null
+          : DateFormat('MMM${separator1}dd${separator2}yy').format(this!);
+
+  /// Default => 2022-05-24 => May-05-2022
+  String? toMmmDdYyyy([String? separator1 = '-', String? separator2 = '-']) =>
+      this == null
+          ? null
+          : DateFormat('MMM${separator1}dd${separator2}yyyy').format(this!);
+
+  //endregion
+
+  //region year month date
+  /// Default => 2022-05-24 => 2022-5-24
+  String? toYMD([String? separator1 = '-', String? separator2 = '-']) =>
+      this == null
+          ? null
+          : DateFormat('y${separator1}M${separator2}d').format(this!);
+
+  /// Default => 2022-05-24 => 22-05-24
+  String? toYyMmDd([String? separator1 = '-', String? separator2 = '-']) =>
+      this == null
+          ? null
+          : DateFormat('yy${separator1}MM${separator2}dd').format(this!);
+
+  /// Default => 2022-05-24 => 2022-05-24
+  String? toYyyyMmDd([String? separator1 = '-', String? separator2 = '-']) =>
+      this == null
+          ? null
+          : DateFormat('yyyy${separator1}MM${separator2}dd').format(this!);
+
+  /// Default => 2022-05-24 => 22-May-24
+  String? toYyMmmDd([String? separator1 = '-', String? separator2 = '-']) =>
+      this == null
+          ? null
+          : DateFormat('yy${separator1}MMM${separator2}dd').format(this!);
+
+  /// Default => 2022-05-24 => 2022-May-24
+  String? toYyyyMmmDd([String? separator1 = '-', String? separator2 = '-']) =>
+      this == null
+          ? null
+          : DateFormat('yyyy${separator1}MMM${separator2}dd').format(this!);
+
+//endregion
+//endregion
+
+//region time
+  /// Result => 7:05 AM
+  String? tohmma() => this == null ? null : DateFormat('h:mm a').format(this!);
+
+  /// Result => 7:05:12 AM
+  String? tohmmssa() =>
+      this == null ? null : DateFormat('h:mm:ss a').format(this!);
+
+  /// Result => 07:05 AM
+  String? tohhmma() =>
+      this == null ? null : DateFormat('hh:mm a').format(this!);
+
+  /// Result => 07:05:12 AM
+  String? tohhmmssa() =>
+      this == null ? null : DateFormat('hh:mm:ss a').format(this!);
+
+  /// Result => 19:05
+  String? toHmm() => this == null ? null : DateFormat('H:mm').format(this!);
+
+  /// Result => 19:05
+  String? toHHmm() => this == null ? null : DateFormat('HH:mm').format(this!);
+
+  /// Result => 19:05:12
+  String? toHHmmss() => this == null ? null : DateFormat('HH:mm').format(this!);
+//endregion time
+}
